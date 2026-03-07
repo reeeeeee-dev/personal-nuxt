649f15dcc65caaff6deadd49058e53aeef738858
Reetik Patel <reetikp@gmail.com>
2026-03-07 14:44:10 -0600

    🚧 ⚗️ home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,15 +5,38 @@ const { ink, cream, red, sage } = useTheme();
 </script>
 
 <template>
-  <div
-    class="absolute bottom-5 w-min text-(--red) text-[20dvw] leading-none px-6"
-  >
-    Reetik Patel
+  <div class="flex justify-between h-dvh">
+    <div
+      class="flex gap-12 w-min h-full items-end text-(--red) text-[20dvw] leading-none px-6"
+    >
+      Reetik Patel
+    </div>
+    <div
+      class="[writing-mode:vertical-lr] rotate-180 w-full text-[23.5dvh] leading-none text-(--cream)"
+    >
+      <div>Developer</div>
+      <div>Designer</div>
+    </div>
   </div>
 
   <div class="absolute top-0 left-0 -z-10">
     <Shader class="h-screen w-screen bg-(--ink)">
-      <Aurora :color-a="red" :color-b="cream" />
+      <Aurora :color-a="ink" :color-b="cream" />
     </Shader>
   </div>
 </template>
+
+<style scoped>
+.portrait {
+  animation: slideIn 0.8s cubic-bezier(0.22, 1, 0.36, 1) 0.5s forwards;
+  transform: translateX(100%);
+  opacity: 0;
+}
+
+@keyframes slideIn {
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+</style>
